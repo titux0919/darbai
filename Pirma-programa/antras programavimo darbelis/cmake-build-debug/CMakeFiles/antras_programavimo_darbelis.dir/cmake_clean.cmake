8194f69eb58f9566527da067cea0915c0913d3f9
titux0919 <titas.saviscevas09@gmail.com>
2024-09-25 09:30:24 +0300

    teorijos naujas failas

---
+++ Pirma-programa/antras programavimo darbelis/cmake-build-debug/CMakeFiles/antras_programavimo_darbelis.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/antras_programavimo_darbelis.dir/main.cpp.obj"
+  "CMakeFiles/antras_programavimo_darbelis.dir/main.cpp.obj.d"
+  "antras_programavimo_darbelis.exe"
+  "antras_programavimo_darbelis.exe.manifest"
+  "antras_programavimo_darbelis.pdb"
+  "libantras_programavimo_darbelis.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/antras_programavimo_darbelis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Pirma-programa/antras programavimo darbelis/cmake-build-debug/CMakeFiles/antras_programavimo_darbelis.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for antras_programavimo_darbelis.
+# This may be replaced when dependencies are built.
