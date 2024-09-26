4dfadee9b175ae6859a8f50fa96eab19f5103002
titux0919 <titas.saviscevas09@gmail.com>
2024-09-26 15:37:50 +0300

    nauja praktine uzduotis

---
+++ Pirma-programa/Funkcijos 09-26/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+void ManoasmenineInformacija () {
+    cout << "Vardas: Titas" << endl;
+    cout << "Pasirinkau programu sistemu programa, nes domiuosi technologijomis ir man idomu suzinoti daugiau apie tai" << endl;
+}
+int main () {
+    ManoasmenineInformacija(); // Pirmasis iškvietimas
+    ManoasmenineInformacija(); // Antrasis iškvietimas
+    return 0;
+}
