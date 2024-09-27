f16a8f8eaf7f24cfb7b0854b27d221a0510fd040
titux0919 <titas.saviscevas09@gmail.com>
2024-09-27 09:07:55 +0300

    atnaujinta

---
+++ Pirma-programa/Funkcijos 09-26/cmake-build-debug/CMakeFiles/Funkcijos.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Funkcijos.dir/main.cpp.obj"
+  "CMakeFiles/Funkcijos.dir/main.cpp.obj.d"
+  "Funkcijos.exe"
+  "Funkcijos.exe.manifest"
+  "Funkcijos.pdb"
+  "libFunkcijos.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Funkcijos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
