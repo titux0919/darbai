0ebaa64daad06df7883cd4379e87fc87746ed550
titux0919 <titas.saviscevas09@gmail.com>
2024-10-23 20:54:44 +0300

    pabaigiau 1 užduotį

---
+++ Pirma-programa/antroji praktine uzduotis/cmake-build-debug/CMakeFiles/antroji_praktine_uzduotis.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/antroji_praktine_uzduotis.dir/main.cpp.obj"
+  "CMakeFiles/antroji_praktine_uzduotis.dir/main.cpp.obj.d"
+  "antroji_praktine_uzduotis.exe"
+  "antroji_praktine_uzduotis.exe.manifest"
+  "antroji_praktine_uzduotis.pdb"
+  "libantroji_praktine_uzduotis.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/antroji_praktine_uzduotis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Pirma-programa/antroji praktine uzduotis/main.cpp
+++ Pirma-programa/antroji praktine uzduotis/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 
 using namespace std;
 
@@ -56,4 +56,4 @@ int main() {
 
     return 0;
 }
-
+/*
