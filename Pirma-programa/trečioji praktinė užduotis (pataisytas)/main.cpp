5fc6f040e978ac294fbe5afcc930274f4fa52f18
titux0919 <titas.saviscevas09@gmail.com>
2024-11-21 15:25:09 +0200

    dar karta papildziau programa

--- Pirma-programa/trečioji praktinė užduotis (pataisytas)/main.cpp
+++ Pirma-programa/trečioji praktinė užduotis (pataisytas)/main.cpp
@@ -76,3 +76,26 @@ string decryptASCII(const string& text, const string& key) {
     }
     return result;
 }
+
+void menu() {
+    int choice, action;
+    string text, key;
+
+    while (true) {
+        cout << "Pasirinkite šifravimo būdą:\n1. Abėcėlės šifravimas\n2. ASCII šifravimas\n0. Baigti\nJūsų pasirinkimas: ";
+        cin >> choice;
+        cin.ignore();
+
+        if (choice == 0) {
+            cout << "Programos pabaiga.\n";
+            break;
+        }
+
+        cout << "Įveskite tekstą: ";
+        getline(cin, text);
+        cout << "Įveskite slaptąjį raktą: ";
+        getline(cin, key);
+
+        cout << "Pasirinkite veiksmą:\n1. Užšifruoti\n2. Atšifruoti\nJūsų pasirinkimas: ";
+        cin >> action;
+        cin.ignore();
