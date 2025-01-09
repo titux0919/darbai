eda68666fb4a487997ef3059b7f8f9210ece2185
titux0919 <titas.saviscevas09@gmail.com>
2025-01-09 22:06:23 +0200

    pabaigiau 1 užduotį

--- Pirma-programa/5 praktine uzduotis (1)/main.cpp
+++ Pirma-programa/5 praktine uzduotis (1)/main.cpp
@@ -25,5 +25,48 @@ int main() {
         }
     }
 
-    
+    cout << "\nLentele:\n";
+    for (int i = 0; i < eilute; ++i) {
+        for (int j = 0; j < stulpelis; ++j) {
+            cout << table[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "\nEiluciu sumos:\n";
+    for (int i = 0; i < eilute; ++i) {
+        int rowSum = 0;
+        for (int j = 0; j < stulpelis; ++j) {
+            rowSum += table[i][j];
+        }
+        cout << "Eilutes " << i + 1 << " suma: " << rowSum << endl;
+    }
+
+    cout << "\nStulpeliu sumos:\n";
+    for (int j = 0; j < eilute; ++j) {
+        int colSum = 0;
+        for (int i = 0; i < stulpelis; ++i) {
+            colSum += table[i][j];
+        }
+        cout << "Stulpelio " << j + 1 << " suma: " << colSum << endl;
+    }
+
+    int maxVal = table[0][0];
+    for (int i = 0; i < eilute; ++i) {
+        for (int j = 0; j < stulpelis; ++j) {
+            if (table[i][j] > maxVal) {
+                maxVal = table[i][j];
+            }
+        }
+    }
+
+    cout << "\nDidziausia reiksme lenteleje: " << maxVal << endl;
+
+    for (int i = 0; i < eilute; ++i) {
+        delete[] table[i];
+    }
+    delete[] table;
+
+    return 0;
+}
 
