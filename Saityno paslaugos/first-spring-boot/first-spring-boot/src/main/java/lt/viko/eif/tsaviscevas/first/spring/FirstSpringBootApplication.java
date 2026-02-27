8eb7380cd7153affe340a1b3abf13556626eae61
titux0919 <titas.saviscevas09@gmail.com>
2026-02-27 10:37:22 +0200

    naujas

---
+++ Saityno paslaugos/first-spring-boot/first-spring-boot/src/main/java/lt/viko/eif/tsaviscevas/first/spring/FirstSpringBootApplication.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.tsaviscevas.first.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootApplication.class, args);
+	}
+
+}
---
+++ Saityno paslaugos/first-spring-boot/first-spring-boot/src/test/java/lt/viko/eif/tsaviscevas/first/spring/FirstSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.tsaviscevas.first.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
